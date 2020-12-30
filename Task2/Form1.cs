15c7ab7007df09d44fe39114566e909abd86c28d
Bartash Yriy <66261160+uyriy18@users.noreply.github.com>
2020-12-30 10:20:38 +0200

    30.12.2020

---
+++ Task2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        List<Point> pointCollection = new List<Point>();
+            
+        public Form1()  
+        {
+            InitializeComponent();
+        }
+
+        void drawTriangle(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(this.Handle);
+            Pen redPen = new Pen(Color.Red);
+            Point p1 = new Point(e.X, e.Y);
+            Rectangle r = new Rectangle(p1.X, p1.Y, 5, 5);
+            g.FillEllipse(new SolidBrush(Color.Red), r);
+            pointCollection.Add(p1);
+            if(pointCollection.Count == 3)
+            {
+                g.DrawLine(redPen, pointCollection[0], pointCollection[1]);
+                g.DrawLine(redPen, pointCollection[1], pointCollection[2]);
+                g.DrawLine(redPen, pointCollection[2], pointCollection[0]);
+                pointCollection.Clear();
+            }
+
+
+        }
+
+        
+    }
+}
